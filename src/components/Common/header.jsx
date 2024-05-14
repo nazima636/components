2df5fe95a825d8239fee726f877e340392d47054
nazima636 <143519098+nazima636@users.noreply.github.com>
2024-05-14 13:49:26 +0530

    edited header

--- src/components/Common/header.jsx
+++ src/components/Common/header.jsx
@@ -5,26 +5,26 @@ import logo from '../images/logo.png';
 
 const Header = () => {
   return (
-    <header className="bg-gray-100 p-4 flex justify-between items-center">
+    <header className=" border-b-[1px] p-4 flex justify-between items-center">
       <div className="flex items-center">
         <img src={logo} alt="Automation Logo" className="h-10 mr-3" />
       </div>
       <nav className="flex space-x-4">
-        <a href="" className="flex items-center space-x-1 hover:text-black">
-          <FaHome />
-          <span>Home</span>
+        <a href="" className="flex items-center flex-col space-x-1  hover:text-black">
+          <FaHome className=' text-2xl' />
+          <span  className=' text-sm'>Home</span>
         </a>
-        <a href="" className="flex items-center space-x-1 hover:text-black">
-          <FaBook />
-          <span>Trainings</span>
+        <a href="" className="flex items-center flex-col space-x-1  hover:text-black">
+          <FaBook className=' text-2xl'/>
+          <span className=' text-sm'>Trainings</span>
         </a>
-        <a href="" className="flex items-center space-x-1 hover:text-black">
-          <FaClipboardList />
-          <span>Records</span>
+        <a href="" className="flex items-center flex-col space-x-1  hover:text-black">
+          <FaClipboardList className=' text-2xl'/>
+          <span className=' text-sm'>Records</span>
         </a>
-        <a href="" className="flex items-center space-x-1 hover:text-black">
-          <FaBriefcase />
-          <span>Internships</span>
+        <a href="" className="flex items-center flex-col space-x-1 hover:text-black">
+          <FaBriefcase className=' text-2xl'/>
+          <span className=' text-sm'>Internships</span>
         </a>
       </nav>
     </header>
